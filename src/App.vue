a6744f32e9f04e5add875e0e15fbd38b624e6b6d
caioptt <caiopereira552@gmail.com>
2022-05-18 23:01:02 -0300

    criacao projeto vinicius

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,90 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+
+    <v-app-bar app color="secondary" dark>
+
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" color="#DCDCDC" app>
+      <v-list-item class="secondary pb-1">
+        <v-list-item-content>
+          <v-list-item-title class="text-h6 white--text pt-3">
+            Tópicos em vigência
+          </v-list-item-title>
+
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-pencil-outline</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>
+              <h3>Tópico 1</h3>
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider class="pt-1 ml-1 mr-1" color="white"></v-divider>
+
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-pencil-outline</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>
+              <h3>Tópico 2 </h3>
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider class="pt-1 ml-1 mr-1" color="white"></v-divider>
+
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-pencil-outline</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>
+              <h3>Tópico 3 </h3>
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <HelloWorld />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
 
 export default {
+
   name: 'App',
+
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+
+  data: () => ({
+
+    drawer: false,
+  }),
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url("https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@500&display=swap");
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,100 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+
+      <v-col>
+        <v-card-text>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec erat sapien, sodales a tortor ut,
+            viverra dictum</h2>
+        </v-card-text>
+        <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec erat sapien, sodales a tortor ut,
+          viverra dictum
+          massa. Praesent luctus neque dui, sed vulputate mi faucibus sed. Fusce commodo enim ac ex malesuada, in
+          suscipit
+          quam faucibus. Curabitur quis lectus metus. Sed fringilla quam et malesuada gravida. Nullam id metus arcu. Sed
+          eu bibendum massa. Maecenas eget lacus eu mi blandit suscipit at eu ipsum. Vestibulum ante ipsum primis in
+          faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum id metus malesuada, commodo ligula sed,
+          aliquet risus. Sed tempus sem id nibh pellentesque interdum.
+
+          Aliquam tincidunt ipsum non est volutpat iaculis. In efficitur sapien et arcu varius, non varius arcu tempus.
+          Cras eu ex sit amet ante mattis semper. Phasellus dapibus mauris nec quam ultrices, id tempor risus blandit.
+          Quisque mollis, purus eget vulputate luctus, diam mi sodales nunc, nec tincidunt ante nunc eget turpis.
+          Suspendisse porttitor dolor id elit iaculis fringilla. Praesent non condimentum lorem, sed finibus eros.
+          Nullam
+          gravida sed dolor ultrices congue. Curabitur ut leo faucibus, pulvinar tortor et, commodo leo. Phasellus ut
+          placerat ipsum. Nunc nec urna dui. In tempor leo quis velit tincidunt pretium. Etiam porttitor, urna lacinia
+          porta lacinia, purus dui laoreet orci, non posuere arcu erat a mi. Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit.
+
+          Aliquam sed elementum enim. Aliquam erat volutpat. Vestibulum scelerisque placerat magna, viverra luctus mi
+          dapibus nec. Vivamus maximus, sapien ut fermentum pulvinar, enim lectus lobortis quam, sed rutrum sapien ipsum
+          eget nibh. In a faucibus felis, eget consectetur risus. Aliquam vulputate facilisis velit, eu lacinia diam
+          accumsan sed. Phasellus in enim rutrum, cursus quam eu, aliquet ante. Donec fringilla nibh vel nunc mattis
+          sagittis. Proin id tristique odio, vel varius felis. Donec a aliquet tellus, ac ultricies turpis. Integer leo
+          mi, porttitor eget augue non, imperdiet aliquam leo. Phasellus interdum eros a nunc dignissim, ut porta dolor
+          rutrum. In hac habitasse platea dictumst.</v-card-text>
+
+      </v-col>
+
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+
+  data: () => ({
+    ecosystem: [
+      {
+        text: 'vuetify-loader',
+        href: 'https://github.com/vuetifyjs/vuetify-loader',
+      },
+      {
+        text: 'github',
+        href: 'https://github.com/vuetifyjs/vuetify',
+      },
+      {
+        text: 'awesome-vuetify',
+        href: 'https://github.com/vuetifyjs/awesome-vuetify',
+      },
+    ],
+    importantLinks: [
+      {
+        text: 'Documentation',
+        href: 'https://vuetifyjs.com',
+      },
+      {
+        text: 'Chat',
+        href: 'https://community.vuetifyjs.com',
+      },
+      {
+        text: 'Made with Vuetify',
+        href: 'https://madewithvuejs.com/vuetify',
+      },
+      {
+        text: 'Twitter',
+        href: 'https://twitter.com/vuetifyjs',
+      },
+      {
+        text: 'Articles',
+        href: 'https://medium.com/vuetify',
+      },
+    ],
+    whatsNext: [
+      {
+        text: 'Explore components',
+        href: 'https://vuetifyjs.com/components/api-explorer',
+      },
+      {
+        text: 'Select a layout',
+        href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
+      },
+      {
+        text: 'Frequently Asked Questions',
+        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+      },
+    ],
+  }),
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
