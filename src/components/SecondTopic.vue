4ae805877278a9033c36f62279551353de73b0d2
caioptt <caiopereira552@gmail.com>
2022-05-23 11:25:01 -0300

    adicao formulário

--- src/components/SecondTopic.vue
+++ src/components/SecondTopic.vue
@@ -1,33 +1,24 @@
 <template>
   <v-container>
-    <v-row class="text-center">
-      <v-card class="mt-8 ml-4 mr-4" tile elevation="5">
-        <v-col> </v-col>
+    <v-row>
+      <v-card width="100%" class="mt-8 ml-4 mr-4" tile elevation="5">
+        <v-col>
+          <v-card-text class="text-center mt-2">
+            <h2>Nos ajude com a sua opinião</h2>
+          </v-card-text>
+        </v-col>
 
         <v-col>
           <v-card-text>
-            Cras eu ex sit amet ante mattis semper. Phasellus dapibus mauris nec
-            quam ultrices, id tempor risus blandit. Quisque mollis, purus eget
-            vulputate luctus, diam mi sodales nunc, nec tincidunt ante nunc eget
-            turpis. Suspendisse porttitor dolor id elit iaculis fringilla.
-            Praesent non condimentum lorem, sed finibus eros. Nullam gravida sed
-            dolor ultrices congue. Curabitur ut leo faucibus, pulvinar tortor
-            et, commodo leo. Phasellus ut placerat ipsum. Nunc nec urna dui. In
-            tempor leo quis velit tincidunt pretium. Etiam porttitor, urna
-            lacinia porta lacinia, purus dui laoreet orci, non posuere arcu erat
-            a mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Aliquam sed elementum enim. Aliquam erat volutpat. Vestibulum
-            scelerisque placerat magna, viverra luctus mi dapibus nec. Vivamus
-            maximus, sapien ut fermentum pulvinar, enim lectus lobortis quam,
-            sed rutrum sapien ipsum eget nibh. In a faucibus felis, eget
-            consectetur risus. Aliquam vulputate facilisis velit, eu lacinia
-            diam accumsan sed. Phasellus in enim rutrum, cursus quam eu, aliquet
-            ante. Donec fringilla nibh vel nunc mattis sagittis. Proin id
-            tristique odio, vel varius felis. Donec a aliquet tellus, ac
-            ultricies turpis. Integer leo mi, porttitor eget augue non,
-            imperdiet aliquam leo. Phasellus interdum eros a nunc dignissim, ut
-            porta dolor rutrum. In hac habitasse platea dictumst.</v-card-text
-          ></v-col
+            <p>Participe da pesquisa :</p>
+            <p class="text-center">
+              <v-btn
+                link
+                href="https://docs.google.com/forms/d/1n2gX6hjmUPlSK136IWlWPCsD_HSwZJ-TzlMhf1nyzRI/viewform?hl=pt-br&hl=pt-br&edit_requested=true"
+                >Formulário</v-btn
+              >
+            </p>
+          </v-card-text></v-col
         >
       </v-card>
     </v-row>
